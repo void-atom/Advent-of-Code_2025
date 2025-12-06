2958ebb310677170d205738df0526e0b74d838b3
Ishwor Basyal <ishwor8basyal@gmail.com>
2025-12-06 18:38:47 +0545

    fetch answers concurrently

--- main.go
+++ main.go
@@ -15,7 +15,25 @@ func main() {
 	file_path := fmt.Sprintf("input_files/day%d.txt", choice)
 	input := internal.ReadFile(file_path)
 
-	fmt.Printf("Day %d. Part 1:  %d\n", choice, solution_day.PartOne(input))
-	fmt.Printf("Day %d. Part 2:  %d\n", choice, solution_day.PartTwo(input))
+	// 1. Create channels to receive the results
+	part1Result := make(chan int)
+	part2Result := make(chan int)
+
+	// 2. Concurrent func calls
+	go func() {
+		part1Result <- solution_day.PartOne(input)
+	}()
+
+	go func() {
+		part2Result <- solution_day.PartTwo(input)
+	}()
+
+	// 3. Receive the results from the channels (this blocks until a result is available)
+	result1 := <-part1Result
+	result2 := <-part2Result
+
+	// 4. Print the results
+	fmt.Printf("Day %d. Part 1: %d\n", choice, result1)
+	fmt.Printf("Day %d. Part 2: %d\n", choice, result2)
 
 }
